2108ab116ae50226cca17c7e9bc6aa5ca49360f0
sorul <cromerovargas2d@gmail.com>
2016-10-09 19:03:37 +0200

    prueba

---
+++ prueba/src/main/java/es/upo/cal/App.java
@@ -0,0 +1,13 @@
+package es.upo.cal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
